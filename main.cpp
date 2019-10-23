b24ca42746592bfbe4666f518d1a7ec42dc657d7
Your Name <shist95@mail.ru>
2019-10-23 09:51:51 +0800

    fix include objects, add {

--- main.cpp
+++ main.cpp
@@ -22,20 +22,23 @@ struct  Cmd_writer
     void increment_obj_counter(string data){
         if(open_for_write){
             obj_counter++;
-            cmd_buff.push_back(data);
+//            cmd_buff.push_back(data);
         } else {
             obj_counter=1;
             open_for_write=true;
             cmd_buff.clear();
-            cmd_buff.push_back(data);
+//            cmd_buff.push_back(data);
         }
      }
     void decrement_obj_counter(string data){
          obj_counter--;
-         if(obj_counter)
+         if(obj_counter){
+             std::cout<<"tut add "<<data<<std::endl;
              cmd_buff.push_back(data);
+         }
          else {
              obj_counter=3;
+             open_for_write=false;
              cmd_buff.push_back(data);
              for(const string& str : cmd_buff){
                  std::cout<<str<<std::endl;
@@ -53,6 +56,7 @@ int main()
         if(strcmp(cmd.c_str(),"{")==0){
             cmd_writer.increment_obj_counter(cmd);
         } else if (strcmp(cmd.c_str(),"}")==0) {
+            std::cout<<"now go in decr func"<<std::endl;
              cmd_writer.decrement_obj_counter(cmd);
         } else if (cmd_writer.open_for_write) {
             cmd_writer.cmd_buff.push_back(cmd);
